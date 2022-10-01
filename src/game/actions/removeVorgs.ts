6fc5bae6b3762525df4b045df6fa3f2f1d7e121b
hawkstein <liamhawks+liamhawks@gmail.com>
2022-10-01 21:36:22 +0100

    [fix] update tile with vorg status

--- src/game/actions/removeVorgs.ts
+++ src/game/actions/removeVorgs.ts
@@ -3,12 +3,29 @@ import { GameContext, GameEvent, VorgType } from "../gameTypes"
 
 const removeVorgs = assign<GameContext, GameEvent>({
   vorgs: (context) => {
+    const destroyed = context.vorgs.filter((vorg) => vorg.health <= 0)
+    const emptySeeds = context.vorgs.filter(
+      (vorg) =>
+        vorg.type === VorgType.Seed &&
+        vorg.storedResources?.some((resource) => resource.amount > 0)
+    )
     const vorgs = context.vorgs.filter(
       (vorg) =>
-        vorg.health > 0 ||
-        (vorg.type !== VorgType.Seed &&
-          vorg.storedResources?.some((resource) => resource.amount > 0))
+        !destroyed.some((dead) => dead.id === vorg.id) &&
+        !emptySeeds.some((seed) => seed.id === vorg.id)
     )
+    destroyed.forEach((dead) => {
+      const tile = context.tiles.find((tile) => tile.vorgId === dead.id)
+      if (tile) {
+        tile.vorgId = undefined
+      }
+    })
+    emptySeeds.forEach((seed) => {
+      const tile = context.tiles.find((tile) => tile.vorgId === seed.id)
+      if (tile) {
+        tile.vorgId = undefined
+      }
+    })
     return vorgs
   },
 })
