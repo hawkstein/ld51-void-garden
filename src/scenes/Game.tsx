23016295f3fcaa2ff788a17a556736babfd49df6
hawkstein <liamhawks+liamhawks@gmail.com>
2022-10-02 19:20:47 +0100

    [feat] adjust game menu

--- src/scenes/Game.tsx
+++ src/scenes/Game.tsx
@@ -1,40 +1,38 @@
-import { Button, Stack } from "@mantine/core"
-import useHud from "../hud/useHud"
+import { Button, Grid } from "@mantine/core"
 import useScenes, { Scenes } from "./useScenes"
 import Board from "../game/Board"
 import { useState } from "react"
+import { ReactComponent as Title } from "./Title.svg"
 
 export default function Game() {
   const gotoScene = useScenes((store) => store.set)
-  const show = useHud((store) => store.show)
   const [paused, setPaused] = useState(false)
   return (
-    <>
-      <h1>Void Garden</h1>
+    <div style={{ display: "flex", flexDirection: "column", height: "100%" }}>
       <Board paused={paused} />
-      <Stack>
-        <Button
-          onClick={() => {
-            gotoScene(Scenes.Menu)
-          }}
-        >
-          Back to main menu
-        </Button>
-        <Button
-          onClick={() => {
-            show(true)
-          }}
-        >
-          Open Options Overlay
-        </Button>
-        <Button
-          onClick={() => {
-            setPaused((previous) => !previous)
-          }}
-        >
-          {paused ? "Resume" : "Pause"}
-        </Button>
-      </Stack>
-    </>
+      <Grid align="center" sx={{ marginTop: "auto" }}>
+        <Grid.Col span="auto">
+          <Button
+            onClick={() => {
+              gotoScene(Scenes.Menu)
+            }}
+          >
+            Back to main menu
+          </Button>
+        </Grid.Col>
+        <Grid.Col span={6}>
+          <Title />
+        </Grid.Col>
+        <Grid.Col span="auto">
+          <Button
+            onClick={() => {
+              setPaused((previous) => !previous)
+            }}
+          >
+            {paused ? "Resume" : "Pause"}
+          </Button>
+        </Grid.Col>
+      </Grid>
+    </div>
   )
 }
